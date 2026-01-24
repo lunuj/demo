6f9ca39398519aa49749c7cddd4be18431907ba6
lunuj <2653050252@qq.com>
2026-01-25 00:02:27 +0800

    ✨ feat: 添加示例程序
    使用主程序和头文件，包含版本信息

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include "main.h"
+
+int main(int argc, char const *argv[])
+{
+    int c = getopt(argc, (char * const *)argv, "v");
+    switch (c)
+    {
+    case 'v':
+        printf("demo mode %s\n", VERSION);
+        exit(0);
+    default:
+        break;
+    }
+    printf("Hello world!\n");
+    return 0;
+}
